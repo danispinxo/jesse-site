e6fccaa36223c61e3e50524b2f56d40fc107ce21
Dani Spinosa <genericpronoun@gmail.com>
2022-11-02 15:17:26 -0400

    adding react router

--- home/src/pages/components/Navbar.js
+++ home/src/pages/components/Navbar.js
@@ -1,11 +1,39 @@
 import * as React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-export default function Navbar () {
+export default function TopNavbar () {
   return (
     <div>
-      <p>
-        Navbar contains links to home, services, about, contact, help&resources
-      </p>
+    <Navbar className="navbar">
+      <Container fluid>
+        <Navbar.Toggle className="toggle-btn" aria-controls="navbarScroll"/>
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0" >
+            <Nav.Link >
+              Home
+            </Nav.Link>
+
+            <Nav.Link >
+              Services
+            </Nav.Link>
+
+            <Nav.Link >
+              About
+            </Nav.Link>
+
+            <Nav.Link >
+              Contact
+            </Nav.Link>
+
+            <Nav.Link >
+              Help & Resources
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </div>
   )
 };
--- home/src/pages/index.js
+++ home/src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
+import TopNavbar from "./components/Navbar";
 import Homepage from "./Homepage";
 import Services from "./Services";
 
 const IndexPage = () => {
   return (
     <div>
+      <TopNavbar />
       <Homepage />
       <Services />      
     </div>

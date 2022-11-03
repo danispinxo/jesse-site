8240d43a69c9bc0ea0756aacfdfb82a0611c4388
Dani Spinosa <genericpronoun@gmail.com>
2022-11-03 14:34:40 -0400

    added contact form

--- home/src/pages/index.js
+++ home/src/pages/index.js
@@ -2,13 +2,23 @@ import * as React from "react";
 import TopNavbar from "./components/Navbar";
 import Homepage from "./Homepage";
 import Services from "./Services";
+import About from "./About";
+import Contact from "./Contact";
+import Guide from "./Guide";
+import Resources from "./HelpAndResources";
+import Footer from "./components/Footer";
 
 const IndexPage = () => {
   return (
     <div>
       <TopNavbar />
       <Homepage />
-      <Services />      
+      <Services />
+      <About />
+      <Contact />
+      <Guide />
+      <Resources />
+      <Footer />      
     </div>
   )
 }

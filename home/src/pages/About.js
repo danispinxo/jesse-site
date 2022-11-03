4fe16c995c9b0b27f1a2ef81ccc85d7a7cf7368a
Dani Spinosa <genericpronoun@gmail.com>
2022-11-03 08:37:26 -0400

    adding about page content

---
+++ home/src/pages/About.js
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+export default function About () {
+  return (
+    <main>
+      <h1>About</h1>
+      <h3>What can you expect from working with me?</h3>
+      <table>
+        <tr>
+          <td>Approach (Theory and politics:)</td>
+          <td>Anti-Oppressive, Art Therapy, Attachment Focused, Existential, Expressive Arts, Mindfulness, Narrative, Psychoanalytic, Relational, Somatic, Strengths Based, Trauma Informed, Compassionate Inquiry.</td>          
+        </tr>
+        <tr>
+          <td>Expertise (Client focus:)</td>
+          <td>
+            <ul>
+              <li>2SLGBTQA+</li>
+              <li>Addiction</li>
+              <li>Anger</li>
+              <li>Anxiety</li>
+              <li>Body Image</li>
+              <li>Burnout/Stress</li>
+              <li>Codependence</li>
+              <li>Coping Skills</li>
+              <li>Divorce & Break-ups</li>
+              <li>Eating Issues</li>
+              <li>Elders/Seniors</li>
+              <li>Family Conflict</li>
+              <li>Grief & Loss</li>
+              <li>Identity Exploration</li>
+              <li>Intimacy & Sex</li>
+              <li>Men's Issues</li>
+              <li>Non-Binary</li>
+              <li>Parents</li>
+              <li>Power Relations</li>
+              <li>Relationship Difficulties</li>
+              <li>Self Esteem & Confidence</li>
+              <li>Sex Work Positive</li>
+              <li>Shame</li>
+              <li>Spiritual</li>
+              <li>Trans-Specific</li>
+              <li>Trauma</li>
+              <li>Women's Issues</li>
+            </ul>
+          </td>          
+        </tr>
+        <tr>
+          <td>Approach (Theory and politics:)</td>
+          <td>Anti-Oppressive, Art Therapy, Attachment Focused, Existential, Expressive Arts, Mindfulness, Narrative, Psychoanalytic, Relational, Somatic, Strengths Based, Trauma Informed, Compassionate Inquiry.</td>          
+        </tr>
+
+      </table>
+    </main>
+  )
+}

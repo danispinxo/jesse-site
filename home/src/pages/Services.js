0f9bd8a3f500a28010731932a381caa0758e953f
Dani Spinosa <genericpronoun@gmail.com>
2022-11-08 09:19:59 -0500

    services formatting

--- home/src/pages/Services.js
+++ home/src/pages/Services.js
@@ -6,9 +6,13 @@ import Footer from "../components/Footer";
 export default function Services () {
   return (
     <main>
-      <h1>Available Services</h1>
+      <div className="services-title-holder">
+        <p className="services-title">
+          Available Services
+        </p>
+      </div>
       
-      <div>
+      <div className="help-list">
         <h3>Therapy can help you take charge.</h3>
         <ul>
           <li>Do you feel distant or disconnected from your partner?</li>
@@ -22,39 +26,64 @@ export default function Services () {
         </ul>
       </div>
 
-      <div>
-        <h3>Psychotherapy Session (Individual)</h3>
-        <p>
-          Psychotherapy for individuals specializing in anxiety and stress, self esteem, relationships difficulties, and personal development.
-          <br/>
-          Build upon what was discussed in previous weeks, or explore new topics or concerns that have taken precedence in your life.
-          <br/>
-          In these sessions you are the expert of your own life. There are no judgements or assumptions made during our time; the focus is on collaborating with you to create a space where your voice, your wants, and your unique perspectives are not only heard, but supported and advocated for.
-          <br/>
-          Pricing: $135 / 50 mins.
-        </p>
+      <div className="section">
+        <div className="section-label">
+          <p className="label-title">Psychotherapy Session</p>
+          <p className="label-subtitle">Individual</p>
+        </div>
+
+        <div className="section-content">
+          <p>
+            Psychotherapy for individuals specializing in anxiety and stress, self esteem, relationships difficulties, and personal development.
+          </p>
+          <p>
+            Build upon what was discussed in previous weeks, or explore new topics or concerns that have taken precedence in your life.
+          </p>
+          <p>
+            In these sessions you are the expert of your own life. There are no judgements or assumptions made during our time; the focus is on collaborating with you to create a space where your voice, your wants, and your unique perspectives are not only heard, but supported and advocated for.
+          </p>
+          <p>
+            Pricing: $135 / 50 mins.
+          </p>          
+        </div>
       </div>
 
-      <div>
-        <h3>Psychotherapy for low-income individuals and students</h3>
-        <p>
-        Affordable rates for low-income individuals as well as undergraduate, masters and practicum students who would benefit from therapeutic services but lack the means and access.
-        <br/>
-        *Sliding scale rates are subject to availability
-        <br/>
-        Pricing: $90-120 / 50 mins.
-        </p>
+      <div className="section">
+        <div className="section-label">
+          <p className="label-title">Psychotherapy Session</p>
+          <p className="label-subtitle">Low-income individuals and students</p>
+        </div>
+
+        <div className="section-content">
+          <p>
+            Psychotherapy for individuals specializing in anxiety and stress, self esteem, relationships difficulties, and personal development.
+          </p>
+          <p>
+            *Sliding scale rates are subject to availability
+          </p>
+          <p>
+            Pricing: $90-120 / 50 mins.
+          </p>          
+        </div>
+
       </div>
 
-      <div>
-        <h3>Art Psychotherapy</h3>
-        <p>
-        Using creative practices as a way to help reframe and (re)examine how we look at ourselves, our relationships, and the world. Art psychotherapy isn't about the end goal of creating “Art,” but about the process.
-        <br/>
-        This process means tapping into parts of the brain that often get overlooked or underused, but actually provide a wealth of knowledge about how we each uniquely experience and perceive the world.
-        <br/>
-        Pricing: $135 / 50 mins.
-        </p>
+      <div className="section">
+        <div className="section-label">
+          <p className="label-title">Art Psychotherapy</p>
+        </div>
+
+        <div className="section-content">
+          <p>
+            Using creative practices as a way to help reframe and (re)examine how we look at ourselves, our relationships, and the world. Art psychotherapy isn't about the end goal of creating “Art,” but about the process.
+          </p>
+          <p>
+            This process means tapping into parts of the brain that often get overlooked or underused, but actually provide a wealth of knowledge about how we each uniquely experience and perceive the world.
+          </p>
+          <p>
+            Pricing: $135 / 50 mins.
+          </p>          
+        </div>
       </div>
 
       <div className="consult-box">

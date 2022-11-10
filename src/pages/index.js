fd013214b797aede8e11475464ab66f87ffb81b1
Dani Spinosa <genericpronoun@gmail.com>
2022-11-10 17:03:19 -0500

    adjusting index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import Resources from "./HelpAndResources";
 import Services from "./Services";
 import Guide from "./Guide";
 
-const IndexPage = () => {
+export default function IndexPage () {
   return (
     <BrowserRouter>
       <TopNavbar />
@@ -24,15 +24,4 @@ const IndexPage = () => {
       </Routes>
   </BrowserRouter>
   )
-}
-
-export default IndexPage
-
-export const Head = () => {
-  <head>
-    <title>Jesse Pajuäär Therapy Studios</title>
-    <style>
-      @import url('https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,500;1,400;1,500&family=Lato:ital,wght@0,100;0,400;0,700;0,900;1,400&family=Playfair+Display:wght@600;700;800;900&display=swap');
-    </style>
-  </head>
-}
+};

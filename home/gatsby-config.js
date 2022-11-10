8ba00a955727112ff16789166e85314a2f086672
Dani Spinosa <genericpronoun@gmail.com>
2022-11-10 13:47:49 -0500

    adding module exception for build

--- home/gatsby-config.js
+++ home/gatsby-config.js
@@ -7,3 +7,18 @@ module.exports = {
     "gatsby-plugin-sass",
   ]
 };
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /@remix-run/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+};

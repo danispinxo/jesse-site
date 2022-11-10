d88ff9fa94de30e8b56a6a6c3a37d71a7a8ee6ed
Jonathon Orsi <jonathon.orsi@gmail.com>
2022-11-10 18:24:54 -0500

    router not need in gatsby for static pages

---
+++ gatsby-ssr.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import TopNavbar from './src/components/Navbar';
+
+// Wraps every page in a component
+export const wrapPageElement = ({ element }) => {
+    return (<>
+        <TopNavbar />
+        {element}
+    </>);
+}

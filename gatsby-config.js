98ced052d5e8881333b4a51fb60dd078d2c84f79
Dani Spinosa <genericpronoun@gmail.com>
2024-06-22 15:08:28 -0400

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     title: "Jesse Pajuaar Therapy Studios",
     siteUrl: "https://jessesitemain.gatsbyjs.io/",
     description:
-      "Toronto-based art therapist and psychotherapist available for teletherapy and remote therapy, phone and video sessions. Servicing the Toronto area and nearby (Barrie, York Region, Caledon, Mississauga, Brampton, Scarborough, Kitchener-Waterloo, Hamilton, Niagara, St. Catharines). Offering psychotherapy and art therapy for teens and adults.",
+      "Toronto-based art therapist and psychotherapist available for teletherapy and remote therapy, phone and video sessions. Servicing the Toronto area and nearby (Barrie, York Region, Caledon, Mississauga, Brampton, Scarborough, Kitchener-Waterloo, Hamilton, Niagara, St. Catharines, Georgian Bay). Offering psychotherapy and art therapy for teens and adults.",
     image: "./src/images/Logo.webp",
   },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-netlify"],

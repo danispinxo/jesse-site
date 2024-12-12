a91bd8ac475dedbfe4b403315bbb39780f825e04
Dani Spinosa <genericpronoun@gmail.com>
2024-12-12 12:53:43 -0500

    add pages

---
+++ src/app/about/page.jsx
@@ -0,0 +1,149 @@
+import "../styles/styles.scss";
+import "../styles/about-and-services.scss";
+import Link from "next/link";
+import Footer from "../components/Footer";
+
+export default function About() {
+  return (
+    <main className="page-content">
+      <div className="about-title-holder">
+        <h1 className="about-title">About</h1>
+      </div>
+
+      <div className="about-expectations">
+        <p>What can you expect from working with me?</p>
+      </div>
+
+      <div className="section">
+        <div className="section-label">
+          <p className="label-title">Approach</p>
+          <p className="label-subtitle">Theory & Politics</p>
+        </div>
+
+        <div className="section-content">
+          Anti-Oppressive, Art Therapy, Attachment Focused, Existential,
+          Expressive Arts, Mindfulness, Narrative, Psychoanalytic, Relational,
+          Somatic, Strengths Based, Trauma Informed, Compassionate Inquiry.
+        </div>
+      </div>
+
+      <div className="section">
+        <div className="section-label">
+          <p className="label-title">Expertise</p>
+          <p className="label-subtitle">Client Focus</p>
+        </div>
+        <div className="section-content">
+          <ul>
+            <li>2SLGBTQA+</li>
+            <li>Addiction</li>
+            <li>Anger</li>
+            <li>Anxiety</li>
+            <li>Body Image</li>
+            <li>Burnout/Stress</li>
+            <li>Codependence</li>
+            <li>Coping Skills</li>
+            <li>Divorce & Break-ups</li>
+            <li>Eating Issues</li>
+            <li>Elders/Seniors</li>
+            <li>Family Conflict</li>
+            <li>Grief & Loss</li>
+            <li>Identity Exploration</li>
+            <li>Intimacy & Sex</li>
+            <li>Men&apos;s Issues</li>
+            <li>Non-Binary</li>
+            <li>Parents</li>
+            <li>Power Relations</li>
+            <li>Relationship Difficulties</li>
+            <li>Self Esteem & Confidence</li>
+            <li>Sex Work Positive</li>
+            <li>Shame</li>
+            <li>Spiritual</li>
+            <li>Trans-Specific</li>
+            <li>Trauma</li>
+            <li>Women&apos;s Issues</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="section">
+        <div className="section-label">
+          <p className="label-title">Personal Statement</p>
+          <p className="label-subtitle">How I Can Help</p>
+        </div>
+        <div className="section-content">
+          <p>
+            Our work together begins with the belief that you are the expert on
+            your own life, and that I am here to help motivate and empower you
+            towards the personal growth that matters most to you. I also believe
+            that our relationships, communities, and social contexts have a
+            significant impact in shaping who we are, what we take notice of in
+            the world, and how we come to engage with that world. I see the
+            therapeutic relationship as no different, and therefore provide a
+            collaborative, nonjudgmental, compassionate, and safe atmosphere
+            where clients feel both heard and seen. In all aspects of therapy, I
+            take a non-pathologizing, culturally sensitive, and anti-oppressive
+            stance that seeks to disrupt and reposition systemically oppressive
+            narratives.
+          </p>
+          <p>
+            Through an integrative approach, I tailor my practice to best meet
+            each client&apos;s unique needs and perspectives while working from
+            an attachment, trauma-informed, and systems-based lens. This may
+            include any combination of art therapy, narrative therapy, somatic
+            therapy (mind-body), gestalt therapy, and family/systems therapy. In
+            short, this means looking closely at meaningful relationships and
+            life events, reauthoring harmful narratives, empowering you through
+            the contextualizing and processing of significant experiences, and
+            working on reconnecting with your emotional and bodily
+            intelligences.
+          </p>
+          <p>
+            For those curious about art therapy, here&apos;s a bit about how I
+            practice. I focus on using creative practices as a way to help
+            reframe and (re)examine how we look at ourselves, our relationships,
+            and the world. Art therapy isn&apos;t about the end goal of creating
+            “Art,” but about the process. This process means tapping into parts
+            of the brain that often get overlooked or underused, but actually
+            provide a wealth of knowledge about how we each uniquely experience
+            and perceive the world. Through the use of imagination, metaphor,
+            symbolism, and associations, art therapy can help clients garner new
+            perspectives, and even help us problem solve in new ways. In short,
+            art therapy provides a nonthreatening means of accessing,
+            externalizing, making sense of, and integrating difficult thoughts,
+            feelings, and experiences when we find that words alone are
+            seemingly not up to the task.
+          </p>
+        </div>
+      </div>
+
+      <div className="section">
+        <div className="section-label">
+          <p className="label-title">
+            Education & <br />
+            Qualifications
+          </p>
+        </div>
+        <div className="section-content">
+          I received my Honours BA in Psychology from York University, and my
+          Masters level diploma in Art Therapy from the Toronto Art Therapy
+          Institute (TATI). I am a fully qualified Registered Psychotherapist
+          with the College of Registered Psychotherapists of Ontario (CRPO), a
+          Professional member of the Canadian Art Therapy Association (CATA),
+          and a member of the Society of Estonian Artists in Toronto (EKKT).
+        </div>
+      </div>
+
+      <div className="consult-box">
+        <div className="consult-text">
+          <p>No Fee Consultation (Individual) - 15 min.</p>
+        </div>
+
+        <Link href="/contact">
+          <button className="consult-btn">Book Now</button>
+        </Link>
+      </div>
+
+      <Footer />
+    </main>
+  );
+}
---
+++ src/app/resources/page.jsx
@@ -0,0 +1,198 @@
+import "../styles/styles.scss";
+import "../styles/resources.scss";
+
+export default function Resources() {
+  return (
+    <main>
+      <div className="resources-title-holder">
+        <p className="resources-title">Mental Health Resources</p>
+      </div>
+
+      <div className="resource-cards">
+        <div className="resource-card">
+          <img
+            className="card-img"
+            src="/images/Kids.webp"
+            alt="Children Playing Tug-of-War"
+          />
+          <p className="card-label">Kids Help Phone</p>
+          <p className="card-content">
+            <a
+              href="https://kidshelpphone.ca/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Kids Help Phone
+            </a>{" "}
+            is a Canadian and world leader known for their expertise and
+            continuous innovation as Canada&apos;s only 24/7 counselling and
+            information service for young people.
+          </p>
+          <p className="card-content">
+            Since 1989, trained & professional counsellors have been listening
+            to kids, often when no one else can or will. &quot;We are always
+            there.&quot;
+          </p>
+        </div>
+
+        <div className="resource-card">
+          <img
+            className="card-img"
+            src="/images/Crisis.webp"
+            alt="Folded Hands"
+          />
+          <p className="card-label">Crisis Services Canada</p>
+          <p className="card-content">
+            Available to all Canadians seeking support. Visit{" "}
+            <a href="https://talksuicide.ca/" target="_blank" rel="noreferrer">
+              Crisis Services Canada
+            </a>{" "}
+            for the distress centres and crisis organizations nearest you.
+          </p>
+          <p className="card-content">
+            If you or someone you know is thinking about suicide, call the
+            Canada Suicide Prevention Service at 1-833-456-4566 (24/7) or text
+            45645 (4 pm to 12 am ET).
+          </p>
+        </div>
+
+        <div className="resource-card">
+          <img
+            className="card-img"
+            src="/images/Indigenous.webp"
+            alt="Multicultural Support Group"
+          />
+          <p className="card-label">Support for Indigenous Communities</p>
+          <p className="card-content">
+            <a
+              href="https://www.hopeforwellness.ca/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Hope For Wellness
+            </a>{" "}
+            is available to all Indigenous peoples across Canada who need
+            immediate crisis intervention. Experienced and culturally sensitive
+            help line counsellors can help if you want to talk or are
+            distressed.
+          </p>
+          <p className="card-content">
+            Call 1-855-242-3310 (toll-free) or connect to the online Hope for
+            Wellness chat.
+          </p>
+          <p className="card-content">
+            Additional resources can be found{" "}
+            <a
+              href="https://www.camh.ca//-/media/files/community-resource-sheets/aboriginal-resources-pdf.pdf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              here
+            </a>
+            .
+          </p>
+        </div>
+
+        <div className="resource-card">
+          <img
+            className="card-img"
+            src="/images/Queer.webp"
+            alt="Pride Parade with Flags"
+          />
+          <p className="card-label">2SLGBTQA+ Support Services</p>
+          <p className="card-content">
+            <a
+              href="https://www.rainbowhealthontario.ca/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Rainbow Health Ontario
+            </a>{" "}
+            (RHO) is a province-wide program of Sherbourne Health that works to
+            promote the health of Ontario&apos;s LGBT2SQ communities and improve
+            their access to services. RHO creates resources, provides
+            information and consultation services, delivers education and
+            training, and supports research to develop evidence-based practice
+            and informed public policy.
+          </p>
+          <p className="card-content">
+            <a href="https://www.the519.org/" target="_blank" rel="noreferrer">
+              The 519
+            </a>{" "}
+            provides a range of services from counselling and queer parenting
+            resources to coming out groups, trans programming, and seniors&apos;
+            support. They also provide free, accommodating and non-judgemental
+            space where individuals, organizations and non-profit groups can
+            meet, organize and work towards their goals.
+          </p>
+          <p className="card-content">
+            Additional resources can be found{" "}
+            <a
+              href="https://rocketmanapp.com/blog/19-organizations-supporting-the-lgbtq-community-in-ontario/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              here
+            </a>
+            .
+          </p>
+        </div>
+
+        <div className="resource-card">
+          <img
+            className="card-img"
+            src="/images/Victim.webp"
+            alt="Two People Holding Hands"
+          />
+          <p className="card-label">Victim Services</p>
+          <p className="card-content">
+            <a href="https://trccmwar.ca/" target="_blank" rel="noreferrer">
+              Toronto Rape Crisis Centre/Multicultural Women Against Rape
+            </a>{" "}
+            is a grassroots, women-run collective working towards a
+            violence-free world by providing anti-oppressive, feminist peer
+            support to survivors of sexual violence.
+          </p>
+          <p className="card-content">
+            Their 24 hour hotline is 416-597-8808 or you can email a counselor
+            at crisis@trccmwar.ca.
+          </p>
+        </div>
+
+        <div className="resource-card">
+          <img
+            className="card-img"
+            src="/images/Families.webp"
+            alt="Woman and Child Holding Hands at Sunset"
+          />
+          <p className="card-label">
+            Assistance for Families, Refugees, and Women Fleeing Violence
+          </p>
+          <p className="card-content">
+            The{" "}
+            <a
+              href="https://www.reddoorshelter.ca/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Red Door Family Shelter
+            </a>{" "}
+            provides emergency shelter and support for women and children
+            affected by domestic abuse, families experiencing a housing crisis,
+            and refugee claimants with nowhere else to turn.
+          </p>
+          <p className="card-content">
+            If you or a family you know requires shelter call Central Family
+            Intake at 416-397-5637.
+          </p>
+          <p className="card-content">
+            If you are a woman seeking shelter from situations of violence or
+            intimate partner violence call their Crisis Line at 416-423-0310
+            (press &lsquo;0&lsquo;), or the Assaulted Women&qpos;s Helpline at
+            1-866-863-0511.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}

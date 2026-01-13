4a9e48db9eea7762ed6d4864c8107be81c228815
danispinxo <genericpronoun@gmail.com>
2026-01-13 14:05:09 -0500

    Extract consts to dry up pages

---
+++ src/app/scripts/constants.js
@@ -0,0 +1,92 @@
+export const specialties = [
+  "2SLGBTQIA+",
+  "Addiction",
+  "Anger Management",
+  "Anxiety",
+  "Bisexuality",
+  "Body Positivity",
+  "Codependency",
+  "Coping Skills",
+  "Creativity",
+  "Depression",
+  "Family Conflict",
+  "Grief",
+  "Infidelity",
+  "Life Coaching",
+  "Life Transitions",
+  "Men's Issues",
+  "Narcissistic Personality",
+  "Peer Relationships",
+  "Racial Identity",
+  "Relationship Issues",
+  "Self Esteem",
+  "Sex-Positive, Kink Allied",
+  "Stress",
+  "Transgender",
+  "Trauma and PTSD",
+  "Women's Issues",
+  "Work-Life Balance",
+];
+
+export const approaches = [
+  {
+    title: "Anti-Oppressive",
+    desc: "Acknowledge and address systemic barriers to mental health care.",
+  },
+  {
+    title: "Art Therapy",
+    desc: "Use creative expression as a tool for self-discovery and healing.",
+  },
+  {
+    title: "Attachment Focused",
+    desc: "Understand how early relationships shape current experiences and behaviors.",
+  },
+  {
+    title: "Compassionate Inquiry",
+    desc: "Gently uncover underlying emotional and psychological patterns.",
+  },
+  {
+    title: "Existential",
+    desc: "Explore the meaning of life and personal values in the therapeutic process.",
+  },
+  {
+    title: "Expressive Arts",
+    desc: "Integrate various art forms to facilitate emotional expression and healing.",
+  },
+  {
+    title: "Identity Exploration",
+    desc: "Support authentic self-discovery and the complex intersections of personal identity.",
+  },
+  {
+    title: "Mindfulness",
+    desc: "Cultivate present-moment awareness to reduce stress and enhance well-being.",
+  },
+  {
+    title: "Narrative",
+    desc: "Reframe personal stories to empower and create new perspectives.",
+  },
+  {
+    title: "Performance & Masculinity",
+    desc: "Examine how we perform gender roles, particularly masculinity, and their impact on emotional expression and relationships.",
+  },
+  {
+    title: "Political & Social Justice",
+    desc: "Recognize how political systems and social structures impact mental health and well-being.",
+  },
+  {
+    title: "Relational",
+    desc: "Emphasize the importance of relationships in shaping emotional health.",
+  },
+  {
+    title: "Somatic",
+    desc: "Address the connection between the body and mind in the healing process.",
+  },
+  {
+    title: "Strengths Based",
+    desc: "Focus on individual strengths to foster resilience and growth.",
+  },
+  {
+    title: "Trauma Informed",
+    desc: "Create a safe space that acknowledges and respects the impact of trauma.",
+  },
+];

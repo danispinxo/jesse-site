3c53f7f93d3aab30ebb6f950567b60fe9f09ea9e
danispinxo <genericpronoun@gmail.com>
2026-05-13 17:07:13 -0400

    Clean up constants

--- src/app/scripts/constants.js
+++ src/app/scripts/constants.js
@@ -1,96 +1,3 @@
-export const specialties = [
-  "2SLGBTQIA+",
-  "Addiction",
-  "Anger Management",
-  "Anxiety",
-  "Bisexuality",
-  "Body Positivity",
-  "Codependency",
-  "Coping Skills",
-  "Creativity",
-  "Depression",
-  "Family Conflict",
-  "Grief",
-  "Infidelity",
-  "Life Coaching",
-  "Life Transitions",
-  "Men's Issues",
-  "Narcissistic Personality",
-  "Peer Relationships",
-  "Racial Identity",
-  "Relationships",
-  "Self Esteem",
-  "Sex-Positive, Kink Allied",
-  "Stress",
-  "Transgender",
-  "Trauma and PTSD",
-  "Women's Issues",
-  "Work-Life Balance",
-];
-
-export const approaches = [
-  {
-    title: "Anti-Oppressive",
-    desc: "Acknowledge and address systemic barriers to mental health care.",
-  },
-  {
-    title: "Art Therapy",
-    desc: "Use various forms creative expression as a tool for self-discovery and healing.",
-  },
-  {
-    title: "Attachment Focused",
-    desc: "Understand how early relationships shape current experiences and behaviors.",
-  },
-  {
-    title: "Compassionate Inquiry",
-    desc: "Gently uncover underlying emotional and psychological patterns.",
-  },
-  {
-    title: "Existential",
-    desc: "Explore the meaning of life and personal values in the therapeutic process.",
-  },
-  {
-    title: "Expressive Arts",
-    desc: "Integrate various art forms to facilitate emotional expression and healing.",
-  },
-  {
-    title: "Identity Exploration",
-    desc: "Support authentic self-discovery and the complex intersections of personal identity.",
-  },
-  {
-    title: "Mindfulness",
-    desc: "Cultivate present-moment awareness to reduce stress and enhance well-being.",
-  },
-  {
-    title: "Narrative",
-    desc: "Reframe personal stories to empower and create new perspectives.",
-  },
-  {
-    title: "Performance & Masculinity",
-    desc: "Examine how gender roles, particularly masculinity, impact  expression and relationships.",
-  },
-  {
-    title: "Political & Social Justice",
-    desc: "Recognize how political systems and social structures impact mental health and well-being.",
-  },
-  {
-    title: "Relational",
-    desc: "Emphasize the importance of relationships in shaping emotional health.",
-  },
-  {
-    title: "Somatic",
-    desc: "Address the connection between the body and mind in the healing process.",
-  },
-  {
-    title: "Strengths Based",
-    desc: "Focus on individual strengths in order to foster resilience and growth.",
-  },
-  {
-    title: "Trauma Informed",
-    desc: "Create a safe space that acknowledges and respects the impact of trauma.",
-  },
-];
-
 export const individualTherapyFeatures = [
   "Flexible scheduling options",
   "Safe and confidential virtual environment",
@@ -142,7 +49,7 @@ export const consultationFeatures = [
 
 export const pricingPlans = [
   {
-    title: "Initial Consultation (Phone)",
+    title: "Initial Consultation",
     price: "0",
     duration: "15 minutes",
     sectionId: "initial-consultation",
@@ -194,12 +101,12 @@ export const pricingPlans = [
     ],
   },
   {
-    title: "EFT for Couples",
+    title: "Couple's Therapy",
     price: "150",
     duration: "50 minutes",
     sectionId: "eft-couples",
     features: [
-      "Emotion-focused couples work",
+      "Emotion-focused",
       "Understand your cycle",
       "Safer connection",
       "Evidence-based EFT",
